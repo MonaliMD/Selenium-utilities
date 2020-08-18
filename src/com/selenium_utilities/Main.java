61dcb2e55b5e548d9a8f8578bbb482f9d520e814
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-18 13:50:18 +1200

    Add clear() to test

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -13,18 +13,30 @@ public class Main {
 
     driver.get("https://www.facebook.com/");
 
+    //Find and get the WebElement corresponding to the Email Address
     WebElement email = driver.findElement(By.id("email"));
+    email.sendKeys("monali@gmail.com");
+    System.out.println("email field set");
 
-
+    //Find and the WebElement corresponding to the password
     WebElement password = driver.findElement(By.name("pass"));
+    password.sendKeys("monali123");
+    System.out.println("password field is set");
 
-        email.sendKeys("monali@gmail.com");
-        password.sendKeys("monali123");
+    //Deleting input values in the text fields
+        email.clear();
+        password.clear();
+        System.out.println("email and password field are cleared");
 
+        //Find the login button
         WebElement login = driver.findElement(By.id("u_0_b"));
-        login.click();
 
-        //email.clear();
-        //password.clear();
+        // Using click method to submit form
+        email.sendKeys("monalimd@123");
+        password.sendKeys("monalimd123");
+        login.click();
+        System.out.println("login done with login button click");
+
+
     }
 }
