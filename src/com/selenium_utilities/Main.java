317164ff4d61fbd6dd1865b08de6acb0e134c4f8
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-20 14:37:37 +1200

    Script for image click

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -1,9 +1,32 @@
 package com.selenium_utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
+
+    System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+        String baseUrl = "https://www.trademe.co.nz/Members/Register.aspx";
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+
+        driver.findElement(By.id("SiteHeader_SiteTabs_homeLink")).click();
+        Thread.sleep(3000);
+
+        System.out.println(driver.getTitle());
+
+
+    if(driver.getTitle().equals("Buy online and sell with NZ's #1 auction & classifieds site | Trade Me")) {
+        System.out.println("We are back at Trademe home page");
+    }else{
+        System.out.println("We are not in Trademe home page");
+    }
+        driver.quit();
 
     }
 }
