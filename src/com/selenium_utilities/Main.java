2ae13f5d10996c29ede08f89cb1b31f6b91afbcb
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-24 16:06:35 +1200

    Add script for multiple actions

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -1,9 +1,38 @@
 package com.selenium_utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.Action;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "https://www.facebook.com/";
+
+        driver.get(baseUrl);
+        WebElement txtUserName = driver.findElement(By.id("email"));
+
+        Actions builder = new Actions(driver);
+        Action seriesOfActions = builder.moveToElement(txtUserName)
+                                        .click().keyDown(txtUserName, Keys.SHIFT)
+                                        .sendKeys(txtUserName,"hello")
+                                        .keyUp(txtUserName, Keys.SHIFT)
+                                        .doubleClick(txtUserName)
+                                        .contextClick()
+                                        .build();
+
+        seriesOfActions.perform();
 
     }
 }
