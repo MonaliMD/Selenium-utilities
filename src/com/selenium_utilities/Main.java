2d744156218e0bdceef2c01f3f72388a98c86227
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-25 10:26:18 +1200

    Script to accept alert

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -1,9 +1,41 @@
 package com.selenium_utilities;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+        // Alert Message handling
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "http://demo.guru99.com/test/delete_customer.php";
+
+        driver.get(baseUrl);
+
+        driver.findElement(By.name("cusid")).sendKeys("53920");
+        driver.findElement(By.name("submit")).submit();
+
+        //Switch to Alert
+
+        Alert alert = driver.switchTo().alert();
+
+        // Capturing alert message.
+
+        String alertMessage = driver.switchTo().alert().getText();
+
+        //Display alert message
+        System.out.println(alertMessage);
+        Thread.sleep(3000);
+
+        //accept alert
+        alert.accept();
+
 
     }
 }
