dcfd10f9e1431ffaad8bda4e1a645e89063e3f86
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-24 21:47:47 +1200

    Add script for upload file

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -1,9 +1,30 @@
 package com.selenium_utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "http://demo.guru99.com/test/upload/";
+
+        driver.get(baseUrl);
+        WebElement uploadElement = driver.findElement(By.id("uploadfile_0"));
+         uploadElement.sendKeys("/Users/monaliwijesinghe/Desktop/test.html");
+
+         driver.findElement(By.id("terms")).click();
+
+         driver.findElement(By.name("send")).click();
+
+         driver.close();
 
     }
 }
