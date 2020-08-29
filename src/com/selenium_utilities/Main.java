0efb67ba1724718e0f3c4348a1ce38eb6e5619ac
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-30 11:21:59 +1200

    Add script to link selection

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -1,9 +1,24 @@
 package com.selenium_utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        String baseUrl = "http://demo.guru99.com/popup.php";
+
+        driver.get(baseUrl);
+
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
     }
 }
