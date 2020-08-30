d45c1f7ddefe676b99887c5d41a837763d3621e9
Monali Wijesinghe <monalimeg@gmail.com>
2020-08-30 20:53:29 +1200

    Control child window

--- src/com/selenium_utilities/Main.java
+++ src/com/selenium_utilities/Main.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
 
         System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
@@ -20,5 +23,28 @@ public class Main {
         driver.manage().window().maximize();
 
         driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
+
+        String mainWindow = driver.getWindowHandle();
+
+        // To handle all new opened window
+        Set<String> w1= driver.getWindowHandles();
+        Iterator<String> i1 = w1.iterator();
+
+        while (i1.hasNext()){
+            String childWindow = i1.next();
+
+            if(!mainWindow.equalsIgnoreCase(childWindow));
+            {
+                driver.switchTo().window(childWindow);
+                //Thread.sleep(3000);
+                driver.findElement(By.name("emailid")).sendKeys("gaurav.3n@gmail.com");
+                driver.findElement(By.name("btnLogin")).click();
+
+                //Close the child window
+                driver.close();
+            }
+        }
+        // Switching to Parent window i.e Main Window.
+        driver.switchTo().window(mainWindow);
     }
 }
